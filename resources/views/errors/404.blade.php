1a2c3fb871b72c717a74d44a1839041c179432bd
Ingo Ruddat <42838916+Ruddat@users.noreply.github.com>
2022-11-27 09:43:21 +0100

    Pizza Template 404 Page hinzugefügt

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,36 @@
+@extends('errors::minimal')
+
+@section('title', __('Not Found'))
+@section('code', '404')
+@section('message', __('Not Found'))
+
+
+<x-frontend.pizza.pages.fontsgoogle></x-frontend.pizza.pages.fontsgoogle>
+
+
+
+<div class="mad-content">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xl-6">
+          <div class="content-element-7 align-center">
+            <h1 class="mad-404-title">404</h1>
+            <h6>We're sorry, but we can't find the page you were looking for.</h6>
+          </div>
+          <p class="align-center">It's probably some thing we've done wrong but now we know about it and <br>
+          we'll try to fix it. Go <a href="#" class="mad-link color-2">Home</a> or try to search:</p>
+          <form class="one-line type-2 sm-button-down">
+            <input type="email" placeholder="Search">
+            <button type="submit" class="btn btn-style-3 btn-small"><i class="material-icons">search</i><span>Search</span></button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <x-frontend.pizza.pages.footer></x-frontend.pizza.pages.footer>
+
+
+  <x-frontend.pizza.pages.java></x-frontend.pizza.pages.java>
+
